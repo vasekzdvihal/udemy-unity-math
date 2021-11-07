2ae941fa8cac37af1968397c1df55030b4bff762
Vášek Zdvihal <zdvihal@gmail.com>
2021-11-07 22:49:56 +0100

    Bit Mask AND Door Key Challange

--- Assets/AttributeManager.cs
+++ Assets/AttributeManager.cs
@@ -1,32 +1,40 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class AttributeManager : MonoBehaviour
 {
-    // TODO https://stackoverflow.com/questions/8447/what-does-the-flags-enum-attribute-mean-in-c
-    public static int MAGIC = 16;
-    public static int INTELLIGENCE = 8;
-    public static int CHARISMA = 4;
-    public static int FLY = 2;
-    public static int INVISIBLE = 1;
-    
+    static public int BLUEKEY = 4;
+    static public int REDKEY = 2;
+    static public int GREENKEY = 1;
+
     public Text attributeDisplay;
-    private int attributes = 0;
+    public int attributes = 0;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("MAGIC")) attributes |= MAGIC;
-        if (other.gameObject.CompareTag("INTELLIGENCE")) attributes |= INTELLIGENCE;
-        if (other.gameObject.CompareTag("CHARISMA")) attributes |= CHARISMA;
-        if (other.gameObject.CompareTag("FLY")) attributes |= FLY;
-        if (other.gameObject.CompareTag("INVISIBLE")) attributes |= INVISIBLE; 
-        if (other.gameObject.CompareTag("ANTIMAGIC")) attributes &= ~MAGIC; 
-        
-        if (other.gameObject.CompareTag("MULTIPLE")) attributes |= (INTELLIGENCE | MAGIC | CHARISMA); 
-        if (other.gameObject.CompareTag("ANTIMULTIPLE")) attributes &= ~(INTELLIGENCE | MAGIC); 
+        if(other.gameObject.tag == "BLUE_KEY")
+        {
+            attributes |= BLUEKEY;
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "RED_KEY")
+        {
+            attributes |= REDKEY;
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "GREEN_KEY")
+        {
+            attributes |= GREENKEY;
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "GOLD_KEY")
+        {
+            attributes |= (BLUEKEY | REDKEY | GREENKEY);
+            Destroy(other.gameObject);
+        }
     }
 
     // Start is called before the first frame update
@@ -40,7 +48,6 @@ public class AttributeManager : MonoBehaviour
     {
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
         attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
-
         attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
     }
        
