4ca09076fca568deff2a4fbc2bc01a3b8b6cd806
Vášek Zdvihal <zdvihal@gmail.com>
2021-11-20 20:12:21 +0100

    Bitboards practice 2.

--- Assets/Scripts/CreateBoard.cs
+++ Assets/Scripts/CreateBoard.cs
@@ -8,12 +8,17 @@ public class CreateBoard : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
     public GameObject housePrefab;
+    public GameObject treePrefab;
     public Text score;
     private long dirtBB = 0;
+    private long treeBB = 0;
+    private long playerBB = 0;
+    private GameObject[] tiles;
     
     // Start is called before the first frame update
     void Start()
     {
+        tiles = new GameObject[64];
         for (int r = 0; r < 8; r++)
         {
             for (int c = 0; c < 8; c++)
@@ -22,6 +27,7 @@ public class CreateBoard : MonoBehaviour
                 Vector3 pos = new Vector3(c, 0, r);
                 GameObject tile = Instantiate(tilePrefabs[randomTile], pos, Quaternion.identity);
                 tile.name = $"{tile.tag}_{r}_{c}";
+                tiles[r * 8 + c] = tile;
                 if (tile.tag == "Dirt")
                 {
                     dirtBB = SetCellState(dirtBB, r, c);
@@ -31,6 +37,20 @@ public class CreateBoard : MonoBehaviour
         }
 
         Debug.Log("Dirt cells = " + CellCount(dirtBB));
+        InvokeRepeating("PlantTree", 1, 1);
+    }
+
+    void PlantTree()
+    {
+        int rr = UnityEngine.Random.Range(0, 8);
+        int rc = UnityEngine.Random.Range(0, 8);
+        if (GetCellState(dirtBB & ~playerBB, rr, rc))
+        {
+            GameObject tree = Instantiate(treePrefab);
+            tree.transform.parent = tiles[rr * 8 + rc].transform;
+            tree.transform.localPosition = Vector3.zero;
+            treeBB = SetCellState(treeBB, rr, rc);
+        }
     }
 
     void PrintBB(string name, long BB)
@@ -42,6 +62,12 @@ public class CreateBoard : MonoBehaviour
     {
         long newBit = 1L << (row * 8 + col);
         return bitboard |= newBit;
+    }
+
+    bool GetCellState(long bitboard, int row, int col)
+    {
+        long mask = 1L << (row * 8 + col);
+        return ((bitboard & mask) != 0);
     }
 
     int CellCount(long bitboard)
@@ -60,6 +86,17 @@ public class CreateBoard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject house = Instantiate(housePrefab);
+                house.transform.parent = hit.collider.gameObject.transform;
+                house.transform.localPosition = Vector3.zero;
+                playerBB = SetCellState(playerBB, (int)hit.collider.transform.position.z, (int)hit.collider.transform.position.x);
+            }
+        }
     }
 }
