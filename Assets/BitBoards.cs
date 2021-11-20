a5e0d995f0b58f9cda1063ab7524e89fbb20673c
Vášek Zdvihal <zdvihal@gmail.com>
2021-11-20 18:44:42 +0100

    Bitboards intro

---
+++ Assets/BitBoards.cs
@@ -0,0 +1,31 @@
+﻿namespace DefaultNamespace
+{
+    public class BitBoards
+    {
+        long SetCellState(long bitboard, int row, int col)
+        {
+            long newBit = 1L << (row * 8 + col);
+            return (bitboard |= newBit);
+        }
+
+        bool GetCellState(long bitboard, int row, int col)
+        {
+            long mask = 1L << (row * 8 + col);
+            return ((bitboard & mask) != 0);
+        }
+
+        int CellCount(long bitboard)
+        {
+            int count = 0;
+            long bb = bitboard;
+
+            while (bb != 0)
+            {
+                bb &= bb - 1;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
