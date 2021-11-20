649418d32f1e3fba731ac0cbb0aa6dd2be6d37e8
Vášek Zdvihal <zdvihal@gmail.com>
2021-11-20 20:54:12 +0100

    Bitboards practice 3.

--- Assets/Scripts/CreateBoard.cs
+++ Assets/Scripts/CreateBoard.cs
@@ -10,15 +10,16 @@ public class CreateBoard : MonoBehaviour
     public GameObject housePrefab;
     public GameObject treePrefab;
     public Text score;
-    private long dirtBB = 0;
-    private long treeBB = 0;
-    private long playerBB = 0;
-    private GameObject[] tiles;
+    private long _dirtBb = 0;
+    private long _desertBb = 0;
+    private long _treeBb = 0;
+    private long _playerBb = 0;
+    private GameObject[] _tiles;
     
     // Start is called before the first frame update
     void Start()
     {
-        tiles = new GameObject[64];
+        _tiles = new GameObject[64];
         for (int r = 0; r < 8; r++)
         {
             for (int c = 0; c < 8; c++)
@@ -27,16 +28,22 @@ public class CreateBoard : MonoBehaviour
                 Vector3 pos = new Vector3(c, 0, r);
                 GameObject tile = Instantiate(tilePrefabs[randomTile], pos, Quaternion.identity);
                 tile.name = $"{tile.tag}_{r}_{c}";
-                tiles[r * 8 + c] = tile;
-                if (tile.tag == "Dirt")
+                _tiles[r * 8 + c] = tile;
+                if (tile.CompareTag($"Dirt"))
                 {
-                    dirtBB = SetCellState(dirtBB, r, c);
-                    PrintBB("Dirt", dirtBB);
+                    _dirtBb = SetCellState(_dirtBb, r, c);
+                    PrintBB("Dirt", _dirtBb);
+                }
+
+                if (tile.CompareTag($"Desert"))
+                {
+                    _desertBb = SetCellState(_desertBb, r, c);
+                    PrintBB("Desert", _desertBb);
                 }
             }
         }
 
-        Debug.Log("Dirt cells = " + CellCount(dirtBB));
+        Debug.Log("Dirt cells = " + CellCount(_dirtBb));
         InvokeRepeating("PlantTree", 1, 1);
     }
 
@@ -44,18 +51,22 @@ public class CreateBoard : MonoBehaviour
     {
         int rr = UnityEngine.Random.Range(0, 8);
         int rc = UnityEngine.Random.Range(0, 8);
-        if (GetCellState(dirtBB & ~playerBB, rr, rc))
+        if (GetCellState(_dirtBb & ~_playerBb, rr, rc))
         {
-            GameObject tree = Instantiate(treePrefab);
-            tree.transform.parent = tiles[rr * 8 + rc].transform;
+            GameObject tree = Instantiate(treePrefab, _tiles[rr * 8 + rc].transform, true);
             tree.transform.localPosition = Vector3.zero;
-            treeBB = SetCellState(treeBB, rr, rc);
+            _treeBb = SetCellState(_treeBb, rr, rc);
         }
     }
 
-    void PrintBB(string name, long BB)
+    void CalculateScore()
     {
-        Debug.Log(name + ": " + Convert.ToString(BB, 2).PadLeft(64, '0'));
+        score.text = $"Score: {CellCount(_dirtBb & _playerBb) * 10 + CellCount(_desertBb & _playerBb) * 2}";
+    }
+
+    void PrintBB(string name, long bb)
+    {
+        Debug.Log($"{name}: {Convert.ToString(bb, 2).PadLeft(64, '0')}");
     }
 
     long SetCellState(long bitboard, int row, int col)
@@ -89,13 +100,23 @@ public class CreateBoard : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
+            if (Camera.main is { })
             {
-                GameObject house = Instantiate(housePrefab);
-                house.transform.parent = hit.collider.gameObject.transform;
-                house.transform.localPosition = Vector3.zero;
-                playerBB = SetCellState(playerBB, (int)hit.collider.transform.position.z, (int)hit.collider.transform.position.x);
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    var o = hit.collider.gameObject;
+                    var position = o.transform.position;
+                    var r = (int)position.z;
+                    var c = (int)position.x;
+                    if (GetCellState((_dirtBb & ~_treeBb) | _desertBb, r, c))
+                    {
+                        GameObject house = Instantiate(housePrefab, hit.collider.gameObject.transform, true);
+                        house.transform.localPosition = Vector3.zero;
+                        _playerBb = SetCellState(_playerBb, r, c);
+                        CalculateScore();
+                    }
+                }
             }
         }
     }
