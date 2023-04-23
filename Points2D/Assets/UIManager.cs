c734525e5f792bdd14a5f44dee37a8578aaf31c1
Vášek Zdvihal <zdvihal@gmail.com>
2023-04-23 12:33:09 +0200

    Vector - DotCross challange

--- Points2D/Assets/UIManager.cs
+++ Points2D/Assets/UIManager.cs
@@ -10,14 +10,22 @@ public class UIManager : MonoBehaviour {
     public Text tankPosition;
     public Text fuelPosition;
     public Text energyAmt;
+    public Text angle;
 
     public void AddEnergy(string amt)
     {
-        int n;
-        if (int.TryParse(amt, out n))
+	    if (int.TryParse(amt, out _))
         {
             energyAmt.text = amt;
         }
+    }
+
+    public void SetAngle(string amt)
+    {
+	    if (!float.TryParse(amt, out var n)) return;
+	    
+	    n *= Mathf.PI / 180.0f;
+	    tank.transform.up = HolisticMath.Rotate(new Coords(tank.transform.up), n, false).ToVector3();
     }
 
 
@@ -25,10 +33,5 @@ public class UIManager : MonoBehaviour {
 	void Start () {
         tankPosition.text = tank.transform.position + "";
         fuelPosition.text = fuel.GetComponent<ObjectManager>().objPosition + "";
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
